032082e96a123d4789c667a0ac4fe701e8e6ff12
Tyler Thames <tylerthames@bellsouth.net>
2026-04-28 19:54:55 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Tyler Thames on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
